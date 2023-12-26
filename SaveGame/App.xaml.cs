3dc26871934a5a9d3c8be3acfa07adc6cefb6329
rohanharikr <rohanharikumar80@gmail.com>
2023-12-26 20:42:41 +0530

    Add project files.

---
+++ SaveGame/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace SaveGame
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
