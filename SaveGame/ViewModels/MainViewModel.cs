392e1cd1fa10998052a460dbd6737f2293ba3c9b
rohanharikr <rohanharikumar80@gmail.com>
2023-12-29 00:08:10 +0530

    fix envs, reset secret

--- SaveGame/ViewModels/MainViewModel.cs
+++ SaveGame/ViewModels/MainViewModel.cs
@@ -23,8 +23,8 @@ namespace SaveGame.ViewModels
         public MainViewModel()
         {
             igdb = new IGDBClient(
-                "0hl10nl85ycx2zmag82ov97o5lv9yw",
-                "ywef3u5ts1t0ebivlq80p8uqqbrjdh"
+                Environment.GetEnvironmentVariable("IGDB_CLIENT_ID"),
+                Environment.GetEnvironmentVariable("IGDB_CLIENT_SECRET")
             );
         }
 
