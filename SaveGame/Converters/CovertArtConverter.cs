bda7bfeb07839c527d228bef0e5049b2bf481629
rohanharikr <rohanharikumar80@gmail.com>
2023-12-29 00:01:23 +0530

    start of search games

---
+++ SaveGame/Converters/CovertArtConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace SaveGame.Converters
+{
+    class CovertArtConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return $"https://images.igdb.com/igdb/image/upload/t_cover_big_2x/{value}.jpg";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- SaveGame/ViewModels/MainViewModel.cs
+++ SaveGame/ViewModels/MainViewModel.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using IGDB;
 using IGDB.Models;
 
 namespace SaveGame.ViewModels
@@ -14,10 +16,22 @@ namespace SaveGame.ViewModels
         string searchQuery = "";
 
         [ObservableProperty]
-        List<Game> searchResults = new();
+        IEnumerable<Game> searchResults;
+
+        IGDBClient igdb;
 
         public MainViewModel()
         {
+            igdb = new IGDBClient(
+                "x",
+                "x"
+            );
+        }
+
+        async partial void OnSearchQueryChanged(string value)
+        {
+            var games = await igdb.QueryAsync<Game>(IGDBClient.Endpoints.Games, query: $"fields *, artworks.*, cover.*; search \"{value}\"; limit 4;");
+            SearchResults = games;
         }
     }
 }
