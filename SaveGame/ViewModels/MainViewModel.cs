702b1157d63ebb357b2e90f67f0985a3fd5fb405
rohanharikr <rohanharikumar80@gmail.com>
2023-12-28 18:37:18 +0530

    mvvm setup, start of main page

---
+++ SaveGame/ViewModels/MainViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using IGDB.Models;
+
+namespace SaveGame.ViewModels
+{
+    partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        string searchQuery = "";
+
+        [ObservableProperty]
+        List<Game> searchResults = new();
+
+        public MainViewModel()
+        {
+        }
+    }
+}
