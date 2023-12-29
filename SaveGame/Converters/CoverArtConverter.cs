cbabfc95804aa1171ab894bc130e09a78ed29df9
rohanharikr <rohanharikumar80@gmail.com>
2023-12-29 12:10:49 +0530

    filename typo

--- SaveGame/Converters/CoverArtConverter.cs
+++ SaveGame/Converters/CoverArtConverter.cs
@@ -9,7 +9,7 @@ using System.Windows.Data;
 
 namespace SaveGame.Converters
 {
-    class CovertArtConverter : IValueConverter
+    class CoverArtConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
