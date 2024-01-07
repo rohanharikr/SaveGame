8d3e274825996d03a2220aa1a349c42955147d3d
rohanharikr <rohanharikumar80@gmail.com>
2024-01-07 16:54:49 +0530

    modal actions

--- SaveGame/Controls/GameDetailModal.xaml.cs
+++ SaveGame/Controls/GameDetailModal.xaml.cs
@@ -23,6 +23,51 @@ namespace SaveGame.Controls
     /// </summary>
     public partial class GameDetailModal : UserControl
     {
+        public ICommand AddToPlay
+        {
+            get { return (ICommand)GetValue(AddToPlayProperty); }
+            set { SetValue(AddToPlayProperty, value); }
+        }
+
+        public static readonly DependencyProperty AddToPlayProperty =
+            DependencyProperty.Register("AddToPlay", typeof(ICommand), typeof(GameDetailModal));
+
+        public ICommand AddToPlaying
+        {
+            get { return (ICommand)GetValue(AddToPlayingProperty); }
+            set { SetValue(AddToPlayingProperty, value); }
+        }
+
+        public static readonly DependencyProperty AddToPlayingProperty =
+            DependencyProperty.Register("AddToPlaying", typeof(ICommand), typeof(GameDetailModal));
+
+        public ICommand AddToPlayed
+        {
+            get { return (ICommand)GetValue(AddToPlayedProperty); }
+            set { SetValue(AddToPlayedProperty, value); }
+        }
+
+        public static readonly DependencyProperty AddToPlayedProperty =
+            DependencyProperty.Register("AddToPlayed", typeof(ICommand), typeof(GameDetailModal));
+
+        public object CommandParameter
+        {
+            get { return GetValue(CommandParameterProperty); }
+            set { SetValue(CommandParameterProperty, value); }
+        }
+
+        public static readonly DependencyProperty CommandParameterProperty =
+            DependencyProperty.Register("CommandParameter", typeof(Game), typeof(GameDetailModal), new PropertyMetadata(null));
+
+        public ICommand Remove
+        {
+            get { return (ICommand)GetValue(RemoveProperty); }
+            set { SetValue(RemoveProperty, value); }
+        }
+
+        public static readonly DependencyProperty RemoveProperty =
+            DependencyProperty.Register("Remove", typeof(ICommand), typeof(GameDetailModal));
+
         public Game Detail
         {
             get { return (Game)GetValue(DetailProperty); }
