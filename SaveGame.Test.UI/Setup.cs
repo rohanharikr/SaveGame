36a2655c83c6898e0c26ac77bfa578a103b79b12
rohanharikr <rohanharikumar80@gmail.com>
2024-01-12 02:03:10 +0530

    refactor

---
+++ SaveGame.Test.UI/Setup.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Appium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SaveGame.Test.UI
+{
+    public class Setup
+    {
+        WindowsDriver<WindowsElement> driver;
+        string appWorkingDirPath;
+        string appPath;
+        AppiumOptions options;
+
+        public Setup()
+        {
+            appWorkingDirPath = Path.GetFullPath(@"../../../../SaveGame/bin/Debug/net8.0-windows");
+            appPath = Path.Combine(appWorkingDirPath, "SaveGame.exe");
+            options = new AppiumOptions();
+            options.AddAdditionalCapability("app", appPath);
+            options.AddAdditionalCapability("appWorkingDir", appWorkingDirPath);
+        }
+
+        [SetUp]
+        public void StartUp()
+        {
+            driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Close();
+        }
+    }
+}
--- SaveGame.Test.UI/Tests.cs
+++ SaveGame.Test.UI/Tests.cs
@@ -1,34 +1,11 @@
+using NUnit.Framework;
 using OpenQA.Selenium.Appium;
 using OpenQA.Selenium.Appium.Windows;
 
 namespace SaveGame.Test.UI
 {
-    public class Tests
+    public class Tests : Setup
     {
-        WindowsDriver<WindowsElement> driver;
-
-        [SetUp]
-        public void Setup()
-        {
-            string appWorkingDirPath = Path.GetFullPath(@"../../../../SaveGame/bin/Debug/net8.0-windows");
-            string appPath = Path.Combine(appWorkingDirPath, "SaveGame.exe");
-
-            AppiumOptions options = new AppiumOptions();
-            options.AddAdditionalCapability("app", appPath);
-            options.AddAdditionalCapability("appWorkingDir", appWorkingDirPath);
-
-            driver = new WindowsDriver<WindowsElement>(
-                new Uri("http://127.0.0.1:4723"),
-                options
-                );
-        }
-
-        [TearDown]
-        public void TearDown()
-        {
-            driver.Close();
-        }
-
         [Test]
         public void CanAddToPlay()
         {
