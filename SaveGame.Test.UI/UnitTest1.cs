308c48a0b12b4ad20a6f57719bffbd0906f787d5
rohanharikr <rohanharikumar80@gmail.com>
2024-01-11 19:05:37 +0530

    init appium ui test suite

---
+++ SaveGame.Test.UI/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SaveGame.Test.UI
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
