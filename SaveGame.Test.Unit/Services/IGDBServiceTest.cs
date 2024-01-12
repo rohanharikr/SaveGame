3366966ecd85e3663e2cb606305cd05428661977
rohanharikr <rohanharikumar80@gmail.com>
2024-01-12 20:30:50 +0530

    start of unit tests

--- SaveGame.Test.Unit/Services/IGDBServiceTest.cs
+++ SaveGame.Test.Unit/Services/IGDBServiceTest.cs
@@ -1,4 +1,6 @@
-﻿using SaveGame.Services;
+﻿using IGDB.Models;
+using SaveGame.Services;
+using SaveGame.Stores;
 
 namespace SaveGame.Test.Unit.Services
 {
@@ -8,14 +10,18 @@ namespace SaveGame.Test.Unit.Services
 
         public IGDBServiceTest()
         {
-            GameStore
-            _igdbService = new IGDBService(this);
+            GameStore gameStore = new GameStore();
+            _igdbService = new IGDBService(gameStore);
         }
 
         [Test]
-        public void SearchReturnsRelevantResults()
+        public async Task SearchReturnsRelevantResults()
         {
-
+            string searchQuery = "Dark";
+            IEnumerable<Game> results = await _igdbService.SearchGame(searchQuery);
+            Assert.That(results.Count(), Is.EqualTo(5));
+            foreach (Game game in results)
+                Assert.That(game.Name, Does.Contain(searchQuery));
         }
     }
 }
