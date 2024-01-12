071cb79c3d78c795422ed6d34753f75ec1e10acb
rohanharikr <rohanharikumar80@gmail.com>
2024-01-12 20:51:38 +0530

    unit test igdb service

--- SaveGame.Test.Unit/Services/IGDBServiceTest.cs
+++ SaveGame.Test.Unit/Services/IGDBServiceTest.cs
@@ -6,11 +6,11 @@ namespace SaveGame.Test.Unit.Services
 {
     public class IGDBServiceTest
     {
-        private IGDBService _igdbService;
+        readonly private IGDBService _igdbService;
 
         public IGDBServiceTest()
         {
-            GameStore gameStore = new GameStore();
+            GameStore gameStore = new(); //Dummy store
             _igdbService = new IGDBService(gameStore);
         }
 
@@ -18,10 +18,44 @@ namespace SaveGame.Test.Unit.Services
         public async Task SearchReturnsRelevantResults()
         {
             string searchQuery = "Dark";
-            IEnumerable<Game> results = await _igdbService.SearchGame(searchQuery);
-            Assert.That(results.Count(), Is.EqualTo(5));
-            foreach (Game game in results)
+            IEnumerable<Game> result = await _igdbService.SearchGame(searchQuery);
+            Assert.That(result.Count(), Is.EqualTo(5));
+            foreach (Game game in result)
                 Assert.That(game.Name, Does.Contain(searchQuery));
+        }
+
+        [Test]
+        public async Task GetsRecentReleases()
+        {
+            IEnumerable<Game> result = await _igdbService.GetRecentReleases();
+            Assert.That(result.Count(), Is.EqualTo(5));
+            int unixTime = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            int yearInUnixTime = 31536000;
+            int pastYearUnixTime = unixTime - yearInUnixTime;
+
+            foreach (Game game in result)
+            {
+                if (game.FirstReleaseDate == null)
+                    continue;
+
+                Assert.That(game.FirstReleaseDate.Value.ToUnixTimeSeconds(), Is.GreaterThan(pastYearUnixTime));
+                Assert.That(game.FirstReleaseDate.Value.ToUnixTimeSeconds(), Is.LessThan(unixTime));
+            }
+        }
+
+        [Test]
+        public async Task GetsUpcomingReleases()
+        {
+            IEnumerable<Game> result = await _igdbService.GetUpcomingReleases();
+            Assert.That(result.Count(), Is.EqualTo(5));
+            int unixTime = (int)DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1)).TotalSeconds;
+            foreach (Game game in result)
+            {
+                if (game.FirstReleaseDate == null)
+                    continue;
+
+                Assert.That(game.FirstReleaseDate.Value.ToUnixTimeSeconds(), Is.GreaterThan(unixTime));
+            }
         }
     }
 }
