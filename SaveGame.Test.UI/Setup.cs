d0559041505bab8083bf33802d15878d0c82ab37
rohanharikr <rohanharikumar80@gmail.com>
2024-01-12 18:24:28 +0530

    add ui test

--- SaveGame.Test.UI/Setup.cs
+++ SaveGame.Test.UI/Setup.cs
@@ -10,7 +10,8 @@ namespace SaveGame.Test.UI
         string appWorkingDirPath;
         string appPath;
 
-        const int MAX_RETRIES = 10;
+        public const int ESCAPE_KEY_CODE = 27;
+        const int MAX_RETRIES = 20;
         TimeSpan RETRY_INTERVAL = TimeSpan.FromSeconds(1);
 
 
@@ -28,10 +29,10 @@ namespace SaveGame.Test.UI
         [SetUp]
         public void StartUp()
         {
-            driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
-
-            //remove db
+            //Remove DB
             File.Delete(Path.Combine(appWorkingDirPath, "Data.db"));
+
+            driver = new WindowsDriver<WindowsElement>(new Uri("http://127.0.0.1:4723"), options);
         }
 
         [TearDown]
--- SaveGame.Test.UI/Tests.cs
+++ SaveGame.Test.UI/Tests.cs
@@ -1,16 +1,33 @@
 using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Interactions;
 
 namespace SaveGame.Test.UI
 {
     public class Tests : Setup
     {
         [Test]
-        public void CanAddToPlay()
+        public void AddToPlay()
         {
             var highRatedGames = WaitForElements("HighRatedGame");
-            highRatedGames.First().Click();
+            var firstHighRatedGame = highRatedGames.First();
+            firstHighRatedGame.Click(); //Get name of game from modal
             string gamename = WaitForElement("GameName").Text;
-            Console.WriteLine(gamename);
+            driver.Keyboard.PressKey(Keys.Escape); //Press escape key to close modal
+            
+            //Add game
+            Actions action = new Actions(driver);
+            action.ContextClick(firstHighRatedGame).SendKeys(Keys.Down).SendKeys(Keys.Enter).Build().Perform();
+
+            //Navigate to play view
+            var playNavButton = driver.FindElementByAccessibilityId("PlayNavButton");
+            playNavButton.Click();
+            var playGames = WaitForElements("PlayGame");
+            
+            //Verify game
+            Assert.That(playGames.Count, Is.EqualTo(1));
+            playGames.First().Click();
+            Assert.That(WaitForElement("GameName").Text, Is.EqualTo(gamename));
         }
     }
 }
