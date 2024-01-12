08cb42d1595be8a5c59e7985a1ad9eb6fbc39d03
rohanharikr <rohanharikumar80@gmail.com>
2024-01-12 19:47:43 +0530

    init unit test project

---
+++ SaveGame.Test.Unit/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace SaveGame.Test.Unit
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
